bdc83c33be28ddd7623c32b1676f5c7beb116371
adhiiisetiawan <adhisetiawan518@gmail.com>
2019-12-04 19:49:07 +0700

    add detail data event and promo

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 var Application = {
     initApplication : function(){
       $(window).load('pageinit','#page-one',function(){
-        Application.initShowMhs();
+        Application.initShowEvent();
+      }),
+      $(document).on('click','#detail-event',function(){
+        var id_tiket = $(this).data('id_tiket');
+        Application.initShowDetailEvent(id_tiket);
       })
     },
-    initShowMhs : function(){
+    initShowEvent : function(){
       $.ajax({
         url: 'https://api-ppk.herokuapp.com/tiket',
         type: 'get',
@@ -18,14 +22,42 @@ var Application = {
           var appendList='';
           for(var i=0; i < dataObject.length; i++){
             var appendlist = '<li><a href=#page-two ?id='+dataObject[i].id_tiket+
-            '"target="_self" id="detail-mhs" data-nimmhs="'+dataObject[i].id_tiket+'"><h2>'+dataObject[i].nama_tiket+
-'</h2><p>'+dataObject[i].harga_tiket+'</p><p><b>'+dataObject[i].harga_tiket+'</b></p></a></li>';
-            $('#list-mhs').append(appendlist);}
-            $('#list-mhs').listview('refresh');
+            '"target="_self" id="detail-event" data-id_tiket="'+dataObject[i].id_tiket+'"><h2>'+dataObject[i].nama_tiket+
+'</h2><p>'+dataObject[i].tanggal_tiket+'</p><p><b>'+dataObject[i].harga_tiket+'</b></p></a></li>';
+            $('#list-event').append(appendlist);}
+            $('#list-event').listview('refresh');
           },
           complete : function(){
             $.mobile.loading('hide');
           }
         });
-    }
+    },
+  initShowDetailEvent: function (id_Tiket) {
+    $.ajax({
+      url: 'https://api-ppk.herokuapp.com/tiket',
+      type: 'get',
+      dataType:"json",
+      beforeSend: function () {
+        $.mobile.loading('show', {
+          text: 'Please waith while retrieving data...',
+          textVisible: true
+        });
+      },
+      success: function (dataObject) {
+        $('#p-nama_event,#p-alamat,#p-tanggal,#p-harga,#p-stok_tiket').empty();
+        for (var i = 0; i < dataObject.length; i++) {
+          if (dataObject[i].id_tiket == id_Tiket) {
+            $('#p-nama_event').append('<b>Nama Event             : </b>' + dataObject[i].nama_tiket);
+            $('#p-alamat').append('<b>Alamat Event            : </b>' + dataObject[i].alamat_tiket);
+            $('#p-tanggal').append('<b>Tanggal            : </b>' + dataObject[i].tanggal_tiket);
+            $('#p-harga').append('<b>Harga    : </b>' + dataObject[i].harga_tiket);
+            $('#p-stok_tiket').append('<b>Stok             : </b>' + dataObject[i].stok_tiket);
+          }
+        }
+      },
+      complete: function () {
+        $.mobile.loading('hide');
+      }
+    });
+  }
   };
---
+++ js/appPromo.js
@@ -0,0 +1,61 @@
+var Application = {
+    initApplication : function(){
+        $(window).load('pageinit','#page-one',function(){
+            Application.initShowEvent();
+        }),
+        $(document).on('click','#detail-mhs',function(){
+            var id_tiket = $(this).data('nimmhs');
+            Application.initShowDetailEvent(id_tiket);
+        })
+    },
+    initShowEvent : function(){
+        $.ajax({
+            url: 'https://api-ppk.herokuapp.com/promo',
+            type: 'get',
+            beforeSend : function() {
+                $.mobile.loading('show',{
+                    text: 'Please wait while retrieving data...',
+                    textVisible : true
+                });
+            },
+            success : function(dataObject) {
+                var appendList='';
+                for(var i=0; i < dataObject.length; i++){
+                    var appendlist = '<li><a href=#page-two ?id='+dataObject[i].id_promo+
+                        '"target="_self" id="detail-mhs" data-nimmhs="'+dataObject[i].id_promo+'"><h2>'+dataObject[i].nama_promo+
+                        '</h2><p>'+dataObject[i].kode_promo+'</p><p><b>'+dataObject[i].diskon+'</b></p></a></li>';
+                    $('#list-event').append(appendlist);}
+                $('#list-event').listview('refresh');
+            },
+            complete : function(){
+                $.mobile.loading('hide');
+            }
+        });
+    },
+    initShowDetailEvent: function (id_Tiket) {
+        $.ajax({
+            url: 'https://api-ppk.herokuapp.com/promo',
+            type: 'get',
+            dataType:"json",
+            beforeSend: function () {
+                $.mobile.loading('show', {
+                    text: 'Please waith while retrieving data...',
+                    textVisible: true
+                });
+            },
+            success: function (dataObject) {
+                $('#p-nama_promo,#p-kode_promo,#p-diskon').empty();
+                for (var i = 0; i < dataObject.length; i++) {
+                    if (dataObject[i].id_promo == id_Tiket) {
+                        $('#p-nama_promo').append('<b>Nama Promo             : </b>' + dataObject[i].nama_promo);
+                        $('#p-kode_promo').append('<b>Kode Promo            : </b>' + dataObject[i].kode_promo);
+                        $('#p-diskon').append('<b>Diskon            : </b>' + dataObject[i].diskon);
+                    }
+                }
+            },
+            complete: function () {
+                $.mobile.loading('hide');
+            }
+        });
+    }
+};
